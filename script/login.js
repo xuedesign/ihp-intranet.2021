3196e210e43bff43d35312a14b2d3976d11a59a2
ElmoWoo <hello@xuedesign.tw>
2021-07-20 18:07:48 +0800

    所內專區版型調整 #20210720

--- script/login.js
+++ script/login.js
@@ -1,3 +1,3 @@
 $(".textinput input").change(function(){""!=$(this).val()?$(this).addClass("filled"):$(this).removeClass("filled")});
 
-    $('[data-action="see-password"]').on("click",function(){el="#"+$(this).attr("data-toggle"),"password"==$(el).attr("type")?($(this).find("i").addClass("glyphicon-eye-close"),$(el).attr("type","text")):($(this).find("i").removeClass("glyphicon-eye-close"),$(el).attr("type","password")),$(el).focus()});
+$('[data-action="see-password"]').on("click",function(){el="#"+$(this).attr("data-toggle"),"password"==$(el).attr("type")?($(this).find("i").addClass("glyphicon-eye-close"),$(el).attr("type","text")):($(this).find("i").removeClass("glyphicon-eye-close"),$(el).attr("type","password")),$(el).focus()});
