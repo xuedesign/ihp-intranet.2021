1ad87f9fd915683d800cfcd94e596b8b768abdd7
ElmoWoo <hello@xuedesign.tw>
2021-08-05 17:45:02 +0800

    各類學術補助單元製作 #20210805

--- script/webset.js
+++ script/webset.js
@@ -1,4 +1,4 @@
-//menu
+//menu by superfish v1.7.0
 loadjs('https://cdn.jsdelivr.net/npm/superfish@1.7.10/dist/js/superfish.min.js', function() {
     jQuery(document).ready(function() {
         jQuery('ul.sf-menu').superfish({
@@ -11,7 +11,7 @@ loadjs('https://cdn.jsdelivr.net/npm/superfish@1.7.10/dist/js/superfish.min.js',
 //menu fixed top
 loadjs("https://xuedesign.github.io/ihp.2020/script/lib/fixed.js");
 
-//banner
+//banner by flickity v2.2.2
 loadjs('https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', function() {
     $('.main-carousel').flickity({
         // options
@@ -30,25 +30,20 @@ loadjs('https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', function() {
 loadjs("https://xuedesign.github.io/ihp.2020/script/lib/abgne-carousel.js");
 
 
-//lazyload
+//lazyload v1.6.0
 loadjs('https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js', function() {
     const observer = lozad();
     observer.observe();
 });
 
-//wow
-//loadjs('https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min.js', function() {
-//    new WOW().init();
-//});
-
-//PrintArea
+//PrintArea v2.4.1
 loadjs('https://cdnjs.cloudflare.com/ajax/libs/PrintArea/2.4.1/jquery.PrintArea.min.js', function() {
     $("#print_button").click(function(){
         $("#myPrintArea").printArea();
       });
 });
 
-//Font+ 
+//Font+ v1.0
 loadjs(['https://xuedesign.github.io/ihp.2020/script/lib/jquery.cookie.js', 'https://xuedesign.github.io/ihp.2020/script/lib/jquery.textresizer.js'], function() {
     jQuery(document).ready( function() {
         jQuery( "#box-fontsize a" ).textresizer({
@@ -61,7 +56,7 @@ loadjs(['https://xuedesign.github.io/ihp.2020/script/lib/jquery.cookie.js', 'htt
 
 });
 
-//tableRwd(https://cdn.jsdelivr.net/npm/basictable/basictable.css)
+//tableRwd v2.0.2(https://cdn.jsdelivr.net/npm/basictable/basictable.css)
 loadjs('https://cdn.jsdelivr.net/npm/basictable/jquery.basictable.min.js', function() {
     $(document).ready(function() {
         $('#table-two-axis').basictable({
@@ -73,17 +68,7 @@ loadjs('https://cdn.jsdelivr.net/npm/basictable/jquery.basictable.min.js', funct
     });  
 });
 
-//tab-View
-loadjs('https://xuedesign.github.io/ihp.2020/script/lib/jquery.tabslet.min.js', function() {
-    $(function(){
-        $('.mytabs').tabslet();
-    });
-});
-
-//lightbox
+//lightbox v3.5.7
 loadjs(['https://cdn.jsdelivr.net/gh/fancyapps/fancybox/dist/jquery.fancybox.min.css', 'https://cdn.jsdelivr.net/gh/fancyapps/fancybox/dist/jquery.fancybox.min.js'], function() {
     
 });
-
-//allcheck
-loadjs("https://xuedesign.github.io/ihp.2020/script/lib/checkall.js");
